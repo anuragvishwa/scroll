69caf1c9539b9d67166e789780f1d8008f6baac4
anurag <anuragcoolkid@gmail.com>
2020-03-08 02:26:46 +0530

    All

--- src/Ds.js
+++ src/Ds.js
@@ -1,6 +1,6 @@
 import React from 'react'
 const button = {
-  backgroundColor: 'rgba(233, 75, 75, 0.81)',
+  backgroundColor: 'rgba(75, 82, 233, 0.81)',
   border: 'none',
   color: 'white',
   padding: '15px 32px',
@@ -8,15 +8,15 @@ const button = {
   textDecoration: 'none',
   fontSize: '16px',
   position: 'absolute',
-  left: '0',
+  right: '0',
   borderRadius: '10px',
   top: '50%'
 }
 
-export const Fab = function Fab() {
+export const Ds = function Fab() {
   return (
-    <a style={button} href="https://anuragvishwa.github.io/hbd/">
-      B'day
+    <a style={button} href="https://competent-meitner-09679a.netlify.com/">
+      Data Science
     </a>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { Fab } from './Fab'
+import { Ds } from './Ds'
+
 import { AppRouter } from './router'
 import * as ServiceWorker from './serviceWorker'
 
 ReactDOM.render(<AppRouter />, document.getElementById('root'))
 ReactDOM.render(<Fab />, document.getElementById('fab'))
+ReactDOM.render(<Ds />, document.getElementById('ds'))
 
 ServiceWorker.register()
